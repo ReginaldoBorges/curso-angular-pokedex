1449d0695d96ff11c11d0d3e4637471d1383c744
reginaldo borges <reginaldo.borges07@gmail.com>
2024-01-01 19:29:12 -0300

    Secao_13_handson_01 Criação do projeto e principais configurações

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterOutlet, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
